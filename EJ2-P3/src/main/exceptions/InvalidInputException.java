5c0f32bd810e124430f9c5e4087f9b34cf0bfa57
jesuusgpmp14 <146329767+jesuusgpmp14@users.noreply.github.com>
2023-12-22 19:35:19 +0100

    Create InvalidInputException.java

---
+++ EJ2-P3/src/main/exceptions/InvalidInputException.java
@@ -0,0 +1,8 @@
+package ISOII_BC.ISOII_BC.exceptions;
+
+public class InvalidInputException extends Exception {
+
+    public InvalidInputException(String message) {
+        super(message);
+    }
+}
