a884ad2f80d147bdaa9c5115cfdaf64597be1a7b
jesuusgpmp14 <146329767+jesuusgpmp14@users.noreply.github.com>
2023-12-22 19:48:38 +0100

    Create InputOutputHandler.java

---
+++ EJ2-P3/src/main/model/InputOutputHandler.java
@@ -0,0 +1,25 @@
+package ISOII_BC.ISOII_BC.model;
+
+import java.util.Scanner;
+
+public class InputOutputHandler {
+
+    private Scanner scanner;
+
+    public InputOutputHandler() {
+        scanner = new Scanner(System.in);
+    }
+
+    public double leerDato(String mensaje) {
+        System.out.println(mensaje);
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Por favor, introduzca un número válido.");
+            scanner.next(); // Descartar entrada no válida
+        }
+        return scanner.nextDouble();
+    }
+
+    public void mostrarResultado(String mensaje) {
+        System.out.println(mensaje);
+    }
+}
